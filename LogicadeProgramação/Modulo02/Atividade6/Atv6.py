8f6a96be4515f69f6a8188c9bfc161824377d71d
Carlos Cavalcanti <carlosgbcj@gmail.com>
2022-10-06 17:12:59 -0300

    Create Atv6.py

---
+++ LogicadeProgramação/Modulo02/Atividade6/Atv6.py
@@ -0,0 +1,10 @@
+import calcIdade
+nome = input("Digite seu nome completo: ")
+while(True):
+    try:
+        anoNasc = int(input("Digite o ano que você nasceu: "))
+        print(nome, "sua idade é", calcIdade.idade(anoNasc))
+        break
+        print("Erro: Você digitou um valor diferente de um inteiro de 4 digítos!")
+    except Exception as erro:
+        print(erro)
