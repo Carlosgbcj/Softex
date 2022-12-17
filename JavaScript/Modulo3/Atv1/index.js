0572281ce8dba4225c1f493310287ff9e50022ea
Carlos Cavalcanti <carlosgbcj@gmail.com>
2022-12-17 18:41:50 -0300

    Update index.js

---
+++ JavaScript/Modulo3/Atv1/index.js
@@ -0,0 +1,16 @@
+
+// alegre os as palavras me maiusculas
+let  nome  =  'Carlos Cavalcanti'
+
+console . log ( nome . toUpperCase )
+
+// ------------------------------------------------ ------------
+// convertendo para inteiro
+
+deixe  cep  =  '52020220'
+parseInt ( cep )
+console . log ( tipo de  cep )
+
+// ------------------------------------------------ -------------
+// mostrando a quantidade de caracter
+console . log ( nome . comprimento )
