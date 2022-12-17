f7c062fe03f93fad38b2d2c17db15dd7ff7a6198
Carlos Cavalcanti <carlosgbcj@gmail.com>
2022-12-17 00:22:49 -0300

    Create escola.js

---
+++ JavaScript/Modulo3/escola.js
@@ -0,0 +1,28 @@
+var  readlineSync  =  require ( 'readline-sync' ) ;
+
+var  nome  =  input ( 'Digite o seu nome: ' ) ;
+
+console . log ( saudacao ( ) ) ;
+
+var  nota1  =  input ( questionFloat ( nome  +  ' informe sua nota 1: ' ) ) ;
+var  nota2  =  input ( questionFloat ( nome  +  ' informe sua nota 2: ' ) ) ;
+var  nota3  =  input ( questionFloat ( nome  +  ' informe sua nota 3: ' ) ) ;
+
+var  mídia  =  mídia ( nota1 ,  nota2 ,  nota3 ) ;
+
+console . log ( nome  +  ' sua média é: '  +  media ) ;
+
+var  aprovação  =  ( mídia )  =>  {
+    if  ( media  >=  7.0 )  return  "Aprovado"
+    senão  retorne  "Reprovado"
+} ;
+
+console . log ( nome  +  'você foi'  +  aprovação ( media ) ) ;
+
+função  saudação ( )  {
+    return  "Olá"  +  nome ;
+}
+
+função  mídia ( n1 ,  n2 ,  n3 )  {
+    retornar  ( n1  +  n2  +  n3 )  /  3 ;
+}
