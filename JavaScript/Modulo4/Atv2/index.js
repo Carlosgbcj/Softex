42a70b8af2d2be0337e1d16ed8c89dc453b20d1e
Carlos Cavalcanti <carlosgbcj@gmail.com>
2022-12-19 19:14:32 -0300

    Update index.js

---
+++ JavaScript/Modulo4/Atv2/index.js
@@ -0,0 +1,11 @@
+
+
+const  expresso  =  requer  ( 'expresso' )
+
+aplicativo  const =  expresso ( ) ;
+
+app . ouvir ( 3333 )
+
+app . get ( '/'  ,  ( solicitação ,  resposta )  =>  {
+    resposta de retorno . enviar  ( 'Helloq Word' )
+} ) ;
