d878e1917fba028d6875cf8541199e29685f31be
Carlos Cavalcanti <carlosgbcj@gmail.com>
2022-12-19 19:14:39 -0300

    Update Atv6.js

---
+++ JavaScript/Modulo4/Atv6/Atv6.js
@@ -0,0 +1,28 @@
+deixar  carro  = {
+    cor : 'Preto' ,
+    marca : 'Fiat' ,
+    ano : 2016
+}
+
+ matriz  const =  [ 10 ,  20 , 30 ]
+
+const  forOf  =  ( )  => {
+    console . log ( 'Demonstrando o for of' )
+    console . log ( '' )
+    for ( const  e  de  array ) {
+        console . registro ( e )
+    }
+
+}
+console . log ( '' )
+
+const  forIn  =  ( )  => {
+    console . log ( 'Demonstrando o for in' )
+    console . log ( '' )
+    for ( const  e  no  carro ) {
+        console . registro ( e )
+    }
+}
+
+console . log ( forOf ( ) )
+console . log ( paraIn ( ) )
